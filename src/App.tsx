3f414948a4f6dd5f1e1e51cda3f323793ac12929
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-11-20 21:13:14 +0200

    update app providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "./app/store";
+
+import HomePage from "./pages/HomePage";
 function App() {
-  return <main>Hello Trendo Nova</main>;
+  const loggedIn = useAppSelector((state) => state.auth.loggedIn);
+  if (!loggedIn) {
+    return <Navigate to={"/login"} />;
+  }
+  return <HomePage />;
 }
 
 export default App;
